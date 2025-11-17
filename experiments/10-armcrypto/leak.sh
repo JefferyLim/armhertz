a79b1023983944f992845590c12fc73d64677131
Jeffery Lim <jl+git@jefferyjlim.com>
2025-11-17 04:12:37 +0000

    Adding static key, various plaintext

---
+++ experiments/10-armcrypto/leak.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+NAME=${1:-test}
+echo $NAME
+
+mkdir -p data/${NAME}
+TOTAL_PHYSICAL_CORES=4
+TOTAL_LOGICAL_CORES=4
+
+# Setup
+samples=40000	    # 30 seconds (1 ms + 14 ms )
+outer=20			# 30 reps
+num_thread=$TOTAL_LOGICAL_CORES
+date=`date +"%m%d-%H%M"`
+
+# Alert
+echo "This script will take about $(((30)*$outer*3/60+10)) minutes. Reduce 'outer' if you want a shorter run."
+
+# Warmup
+#stress-ng -q --cpu $TOTAL_LOGICAL_CORES -t 10m
+
+# Run
+sudo rm -rf out
+mkdir out
+sudo rm -rf input.txt
+
+for selector in `seq 0 64`; do
+	echo $selector >> input.txt
+done
+
+sudo ./bin/driver_aes_leak ${num_thread} ${samples} ${outer}
+cp -r out data/${NAME}/leak-${date}
